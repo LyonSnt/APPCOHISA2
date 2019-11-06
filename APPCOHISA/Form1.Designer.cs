5c950c3dedc7cfd20084dd9ab5ee59d91278baed
LyonSnt <lyon12189@gmail.com>
2019-11-05 19:13:50 -0500

    Agregar archivos de proyecto.

---
+++ APPCOHISA/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace APPCOHISA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(318, 187);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(75, 23);
+            this.btnInicio.TabIndex = 0;
+            this.btnInicio.Text = "button1";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInicio);
+            this.Name = "Form1";
+            this.Text = "Portada";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInicio;
+    }
+}
+
---
+++ APPCOHISA/Menu.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APPCOHISA
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void contenedor_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnMaximizar.Visible = false;
+            btnRestaurar.Visible = true;
+
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnRestaurar.Visible = false;
+            btnMaximizar.Visible = true;
+
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void barratitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+             ReleaseCapture();
+
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+         
+        }
+
+        private void btnLexico_Click(object sender, EventArgs e)
+        {
+            panelsubmeu.Visible = true;
+
+        }
+
+        private void btncrear_Click(object sender, EventArgs e)
+        {
+            panelsubmeu.Visible = false;
+        }
+
+
+        private void btnVer_Click(object sender, EventArgs e)
+        {
+            panelsubmeu.Visible = false;
+        }
+
+        private void btnSintactico_Click(object sender, EventArgs e)
+        {
+            panelsub2.Visible = true;
+        }
+
+        private void btncrear2_Click(object sender, EventArgs e)
+        {
+            panelsub2.Visible = false;
+        }
+
+        private void btnver2_Click(object sender, EventArgs e)
+        {
+            panelsub2.Visible = false;
+        }
+    }
+
+
+}
